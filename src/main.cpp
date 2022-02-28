e2a423a558638d74e21dfecac1427cfd39bbb9cf
Nutthawut Kongsopa <nutthawutkongsopa@gmail.com>
2022-02-28 10:55:17 +0700

    include getch

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "../libs/my_lib/src/say.h"
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
     string name;
+    int x;
     std::cout << "Hello World!!" << std::endl;
     cout << "Enter your name : ";
     std::cin >> name;
     greeting(name);
+    getch();
 }
